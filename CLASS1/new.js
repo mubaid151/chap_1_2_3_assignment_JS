1764851a9eb679790d1f6a42be0793e27669426e
Mohammad Ubaid <ubaidmohammad151@gmail.com>
2024-08-18 22:39:23 +0500

    Add files via upload

---
+++ CLASS1/new.js
@@ -0,0 +1,49 @@
+// ASSINGMENT NO 1 Chapter 1 "alerts"
+
+alert("Error! Please enter a valid password");
+
+alert("Welcome to JS land....\n Happy Coding!");
+
+alert("hello i can run JS through console");
+console.log(alert);
+
+
+// chapter 2 "Variables"
+ var username = 10;
+ var myName = "mohammad ubaid";
+ console.log(myName);
+
+ var titled = "hello world";
+ alert(titled);
+ 
+ var name1 = "Mohammad Ubaid";
+ alert(name1);
+
+ var age = "20 years old";
+ alert(age);
+
+ var certification = "certified by saylani mass it training center";
+ alert(certification);
+
+ var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+ alert(pizza);
+
+ var email = "My email address is example151@gmail.com";
+ alert(email);
+
+ var book = "A smarter way to learn JS";
+ alert(book);
+
+ var design = "----------^^^*$$$*^^^-------------";
+ alert(design);
+
+
+//  Chapter 3 "Variables and Numbers"
+
+var age = "I am 20 years old";
+alert(age);
+
+var number = "You have visited this website 10 times";
+alert(number);
+
+var birthYear = "My birthday year is 2010";
